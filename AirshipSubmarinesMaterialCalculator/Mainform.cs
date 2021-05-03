238028509c5d4da15d99c8ecb60ec6f20afceb4c
kuranadomoe <Wearestardust404@outlook.com>
2021-05-03 21:28:57 +0800

    写了点爬取相关代码

--- AirshipSubmarinesMaterialCalculator/Mainform.cs
+++ AirshipSubmarinesMaterialCalculator/Mainform.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using HtmlAgilityPack;
@@ -16,6 +17,47 @@ namespace Kuranado.Moe.FFXIV
         public Mainform()
         {
             InitializeComponent();
+            var test = new HtmlWeb().Load(@"https://ff14.huijiwiki.com/wiki/%E7%89%A9%E5%93%81:%E6%97%A0%E6%95%8C%E7%BA%A7%E8%88%B9%E4%BD%93").DocumentNode;
+            var xp1 = @"//div[@class=""infobox-item ff14-content-box""]";
+            var xp2 = @"//div[@class=""item-craft-table-list filter-div""]//table/tr[4]/td/ul/li/ul";
+            var node1 = test.SelectSingleNode(xp1);
+            var node2 = test.SelectSingleNode(xp2);
+            //System.IO.File.WriteAllText("test.html", test.OuterHtml);
+            //System.IO.File.WriteAllText("test1.html", node1.OuterHtml);
+            //System.IO.File.WriteAllText("test2.html", node2.OuterHtml);
+            var result = new TroopExploration.PartInfo();
+            var reg = new Regex(@"等级：(?<lv>\d+)[^配]+
+                                  配件重量：(?<weight>\d+)[^探]+
+                                  探索性能：(?<explore>[\d+\-]+)[^收]+
+                                  收集性能：(?<collection>[\d+-]+)[^巡]+
+                                  巡航速度：(?<speed>[\d+-]+)[^航]+
+                                  航行距离：(?<distance>[\d+-]+)[^恩]+
+                                  恩惠：(?<lucky>[\d+-]+)[^魔]+
+                                  魔导机械修理材料×(?<repair>\d+)",
+                                  RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.IgnorePatternWhitespace);
+            var match = reg.Match(node1.OuterHtml);
+            MessageBox.Show("等级" + match.Groups["lv"].Value + System.Environment.NewLine +
+           "配件重量" + match.Groups["weight"].Value + System.Environment.NewLine +
+           "探索性能" + match.Groups["explore"].Value + System.Environment.NewLine +
+           "收集性能" + match.Groups["collection"].Value + System.Environment.NewLine +
+           "巡航速度" + match.Groups["speed"].Value + System.Environment.NewLine +
+           "航行距离" + match.Groups["distance"].Value + System.Environment.NewLine +
+           "恩惠" + match.Groups["lucky"].Value + System.Environment.NewLine +
+           "修理材料" + match.Groups["repair"].Value);
+
+            var reg2 = new Regex(@" (?<name>[\u4e00-\u9fa5])\s*?    //物品名称
+                                    ×(?<num>\d+)\s*?               //数量
+                                    \[
+                                    [\u4e00-\u9fa5:\d\s★]+           //必定存在至少一个
+/?\s*?(?<cost>\d+)G
+\]",
+                                  RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.IgnorePatternWhitespace);
+            var testOutput = "";
+            foreach (var node in node2.ChildNodes)
+            {
+                testOutput += node.ChildNodes[0].InnerText + System.Environment.NewLine;
+            }
+            MessageBox.Show(testOutput);
         }
     }
 }
--- AirshipSubmarinesMaterialCalculator/TroopExploration.cs
+++ AirshipSubmarinesMaterialCalculator/TroopExploration.cs
@@ -1,8 +1,10 @@
-﻿using System;
+﻿using HtmlAgilityPack;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Xml;
 
@@ -26,7 +28,7 @@ namespace Kuranado.Moe.FFXIV
             /// <summary>
             /// 装备该部件所需的潜艇/飞艇等级
             /// </summary>
-            public byte UsingLev;
+            public byte UsingLv;
             /// <summary>
             /// 该部件占用的重量
             /// </summary>
@@ -54,7 +56,7 @@ namespace Kuranado.Moe.FFXIV
             /// <summary>
             /// 恩惠属性
             /// </summary>
-            public short Luck;
+            public short Lucky;
             /// <summary>
             /// 制作素材列表
             /// </summary>
@@ -84,135 +86,148 @@ namespace Kuranado.Moe.FFXIV
         #region Constant Data
         private const string rootLink = @"https://ff14.huijiwiki.com/wiki/物品:";
         private static readonly string dataPath = Path.Combine(CommonSetting.WikiDataDir, "TroopExplorationData.xml");
-        private static readonly string[,] airshipParts =
+        private static readonly string[][] airshipParts =
         {
-            {"野马级船体","野马级气囊","野马级船首","野马级船尾"},
-            {"无敌级船体","无敌级螺旋桨","无敌级船首","无敌级船尾"},
-            {"企业级船体","企业级气囊","企业级船首","企业级船尾"},
-            {"无敌改级船体","无敌改级螺旋桨","无敌改级船首","无敌改级船尾"},
-            {"奥德赛级船体","奥德赛级气囊","奥德赛级船首","奥德赛级船尾"},
-            {"塔塔诺拉级船体","塔塔诺拉级螺旋桨","塔塔诺拉级船首","塔塔诺拉级船尾"},
-            {"威尔特甘斯级船体","威尔特甘斯级灵翼","威尔特甘斯级船首","威尔特甘斯级船尾"}
+            new string[]{"野马级船体","野马级气囊","野马级船首","野马级船尾"},
+            new string[]{"无敌级船体","无敌级螺旋桨","无敌级船首","无敌级船尾"},
+            new string[]{"企业级船体","企业级气囊","企业级船首","企业级船尾"},
+            new string[]{"无敌改级船体","无敌改级螺旋桨","无敌改级船首","无敌改级船尾"},
+            new string[]{"奥德赛级船体","奥德赛级气囊","奥德赛级船首","奥德赛级船尾"},
+            new string[]{"塔塔诺拉级船体","塔塔诺拉级螺旋桨","塔塔诺拉级船首","塔塔诺拉级船尾"},
+            new string[]{"威尔特甘斯级船体","威尔特甘斯级灵翼","威尔特甘斯级船首","威尔特甘斯级船尾"}
         };
-        private static readonly string[,] submarineParts =
+        private static readonly string[][] submarineParts =
         {
-            {"鲨鱼级船体","鲨鱼级船尾","鲨鱼级船首","鲨鱼级舰桥"},
-            {"甲鲎级船体","甲鲎级船尾","甲鲎级船首","甲鲎级舰桥"},
-            {"须鲸级船体","须鲸级船尾","须鲸级船首","须鲸级舰桥"},
-            {"腔棘鱼级船体","腔棘鱼级船尾","腔棘鱼级船首","腔棘鱼级舰桥"},
-            {"希尔德拉级船体","希尔德拉级船尾","希尔德拉级船首","希尔德拉级舰桥"},
-            {"鲨鱼改级船体","鲨鱼改级船尾","鲨鱼改级船首","鲨鱼改级舰桥"},
-            {"甲鲎改级船体","甲鲎改级船尾","甲鲎改级船首","甲鲎改级舰桥"},
-            {"须鲸改级船体","须鲸改级船尾","须鲸改级船首","须鲸改级舰桥"},
-            {"腔棘鱼改级船体","腔棘鱼改级船尾","腔棘鱼改级船首","腔棘鱼改级舰桥"},
-            {"希尔德拉改级船体","希尔德拉改级船尾","希尔德拉改级船首","希尔德拉改级舰桥"}
+            new string[]{"鲨鱼级船体","鲨鱼级船尾","鲨鱼级船首","鲨鱼级舰桥"},
+            new string[]{"甲鲎级船体","甲鲎级船尾","甲鲎级船首","甲鲎级舰桥"},
+            new string[]{"须鲸级船体","须鲸级船尾","须鲸级船首","须鲸级舰桥"},
+            new string[]{"腔棘鱼级船体","腔棘鱼级船尾","腔棘鱼级船首","腔棘鱼级舰桥"},
+            new string[]{"希尔德拉级船体","希尔德拉级船尾","希尔德拉级船首","希尔德拉级舰桥"},
+            new string[]{"鲨鱼改级船体","鲨鱼改级船尾","鲨鱼改级船首","鲨鱼改级舰桥"},
+            new string[]{"甲鲎改级船体","甲鲎改级船尾","甲鲎改级船首","甲鲎改级舰桥"},
+            new string[]{"须鲸改级船体","须鲸改级船尾","须鲸改级船首","须鲸改级舰桥"},
+            new string[]{"腔棘鱼改级船体","腔棘鱼改级船尾","腔棘鱼改级船首","腔棘鱼改级舰桥"},
+            new string[]{"希尔德拉改级船体","希尔德拉改级船尾","希尔德拉改级船首","希尔德拉改级舰桥"}
         };
+
+        private const string partInfoPattern = @"等级：(?<lv>\d+)[^配]+
+                                                 配件重量：(?<weight>\d+)[^探]+
+                                                 探索性能：(?<explore>[\d+\-]+)[^收]+
+                                                 收集性能：(?<collection>[\d+-]+)[^巡]+
+                                                 巡航速度：(?<speed>[\d+-]+)[^航]+
+                                                 航行距离：(?<distance>[\d+-]+)[^恩]+
+                                                 恩惠：(?<lucky>[\d+-]+)[^魔]+
+                                                 魔导机械修理材料×(?<repair>\d+)";
         #endregion
 
-        #region 废案代码
-        //private static PartInfo[,] _airship = null;
-        //private static PartInfo[,] _submarine = null;
-
-        ///// <summary>
-        ///// 飞空艇船体(第一格)
-        ///// </summary>
-        //public static PartInfo[] AirshipHull
-        //{
-        //    get => throw new NotImplementedException("未实现");
-        //    private set => throw new NotImplementedException("未实现");
-        //}
-        ///// <summary>
-        ///// 飞空艇舾装(第二格)
-        ///// </summary>
-        //public static PartInfo[] AirshipOutfitting
-        //{
-        //    get => throw new NotImplementedException("未实现");
-        //    private set => throw new NotImplementedException("未实现");
-        //}
-        ///// <summary>
-        ///// 飞空艇船首(第三格)
-        ///// </summary>
-        //public static PartInfo[] AirshipBow
-        //{
-        //    get => throw new NotImplementedException("未实现");
-        //    private set => throw new NotImplementedException("未实现");
-        //}
-        ///// <summary>
-        ///// 飞空艇船尾(第四格)
-        ///// </summary>
-        //public static PartInfo[] AirshipStern
-        //{
-        //    get => throw new NotImplementedException("未实现");
-        //    private set => throw new NotImplementedException("未实现");
-        //}
-
-        ///// <summary>
-        ///// 潜艇船体(第一格)
-        ///// </summary>
-        //public static PartInfo[] SubmarineHull
-        //{
-        //    get => throw new NotImplementedException("未实现");
-        //    private set => throw new NotImplementedException("未实现");
-        //}
-        ///// <summary>
-        ///// 潜艇船尾(第二格)
-        ///// </summary>
-        //public static PartInfo[] SubmarineStern
-        //{
-        //    get => throw new NotImplementedException("未实现");
-        //    private set => throw new NotImplementedException("未实现");
-        //}
-        ///// <summary>
-        ///// 潜艇船首(第三格)
-        ///// </summary>
-        //public static PartInfo[] SubmarineBow
-        //{
-        //    get => throw new NotImplementedException("未实现");
-        //    private set => throw new NotImplementedException("未实现");
-        //}
-        ///// <summary>
-        ///// 潜艇舰桥(第四格)
-        ///// </summary>
-        //public static PartInfo[] SubmarineBridge
-        //{
-        //    get => throw new NotImplementedException("未实现");
-        //    private set => throw new NotImplementedException("未实现");
-        //}
-        #endregion
-
+        #region Properties
         private static PartInfo[,] _airship = null;
         private static PartInfo[,] _submarine = null;
 
         /// <summary>
-        /// 获取指定代号的飞空艇套装
+        /// 飞空艇船体(第一格)
         /// </summary>
-        /// <param name="code">
-        /// <para>4位的数字代号,例如2571指:</para>
-        /// <para>2号图的无敌级船体</para>
-        /// <para>5号图的奥德赛级气囊</para>
-        /// <para>7号图的威尔特甘斯级船首</para>
-        /// <para>1号图的野马级船尾</para>
-        /// <para>另外,0代表此项空缺</para>
-        /// </param>
-        /// <returns></returns>
-        public static PartInfo?[] ArishipSuit(string code)
+        public static PartInfo[] AirshipHull
         {
-            return null;
+            get => throw new NotImplementedException("未实现");
+            private set => throw new NotImplementedException("未实现");
         }
         /// <summary>
-        /// 获取指定代号的潜水艇套装
+        /// 飞空艇舾装(第二格)
         /// </summary>
-        /// <param name="code">
-        /// <para>4~8位的数字和'+'组成的代号</para>
-        /// <para>一位数字后面最多只能有一个'+'</para>
-        /// <para>'+'代表改级</para>
-        /// <para>另外,0代表此项空缺</para>
-        /// </param>
-        /// <returns></returns>
-        public static PartInfo?[] SubmarineSuit(string code)
+        public static PartInfo[] AirshipOutfitting
         {
-            return null;
+            get => throw new NotImplementedException("未实现");
+            private set => throw new NotImplementedException("未实现");
         }
+        /// <summary>
+        /// 飞空艇船首(第三格)
+        /// </summary>
+        public static PartInfo[] AirshipBow
+        {
+            get => throw new NotImplementedException("未实现");
+            private set => throw new NotImplementedException("未实现");
+        }
+        /// <summary>
+        /// 飞空艇船尾(第四格)
+        /// </summary>
+        public static PartInfo[] AirshipStern
+        {
+            get => throw new NotImplementedException("未实现");
+            private set => throw new NotImplementedException("未实现");
+        }
+
+        /// <summary>
+        /// 潜艇船体(第一格)
+        /// </summary>
+        public static PartInfo[] SubmarineHull
+        {
+            get => throw new NotImplementedException("未实现");
+            private set => throw new NotImplementedException("未实现");
+        }
+        /// <summary>
+        /// 潜艇船尾(第二格)
+        /// </summary>
+        public static PartInfo[] SubmarineStern
+        {
+            get => throw new NotImplementedException("未实现");
+            private set => throw new NotImplementedException("未实现");
+        }
+        /// <summary>
+        /// 潜艇船首(第三格)
+        /// </summary>
+        public static PartInfo[] SubmarineBow
+        {
+            get => throw new NotImplementedException("未实现");
+            private set => throw new NotImplementedException("未实现");
+        }
+        /// <summary>
+        /// 潜艇舰桥(第四格)
+        /// </summary>
+        public static PartInfo[] SubmarineBridge
+        {
+            get => throw new NotImplementedException("未实现");
+            private set => throw new NotImplementedException("未实现");
+        }
+        #endregion
+
+        #region 废案代码
+
+        //private static PartInfo[,] _airship = null;
+        //private static PartInfo[,] _submarine = null;
+
+        ///// <summary>
+        ///// 获取指定代号的飞空艇套装
+        ///// </summary>
+        ///// <param name="code">
+        ///// <para>4位的数字代号,例如2571指:</para>
+        ///// <para>2号图的无敌级船体</para>
+        ///// <para>5号图的奥德赛级气囊</para>
+        ///// <para>7号图的威尔特甘斯级船首</para>
+        ///// <para>1号图的野马级船尾</para>
+        ///// <para>另外,0代表此项空缺</para>
+        ///// </param>
+        ///// <returns></returns>
+        //public static PartInfo?[] ArishipSuit(string code)
+        //{
+        //    return null;
+        //}
+        ///// <summary>
+        ///// 获取指定代号的潜水艇套装
+        ///// </summary>
+        ///// <param name="code">
+        ///// <para>4~8位的数字和'+'组成的代号</para>
+        ///// <para>一位数字后面最多只能有一个'+'</para>
+        ///// <para>'+'代表改级</para>
+        ///// <para>另外,0代表此项空缺</para>
+        ///// </param>
+        ///// <returns></returns>
+        //public static PartInfo?[] SubmarineSuit(string code)
+        //{
+        //    return null;
+        //}
+
+        #endregion
 
         /// <summary>
         /// <para>从wiki爬取并刷新飞空艇和潜艇部件信息</para>
@@ -220,7 +235,23 @@ namespace Kuranado.Moe.FFXIV
         /// </summary>
         public static void Reflush()
         {
-
+            var web = new HtmlWeb();
+            foreach (var suit in airshipParts)
+            {
+                foreach (var partName in suit)
+                {
+                    var partInfo = CrawlPage(partName);
+                    partInfo.Name = partName;
+                }
+            }
+            foreach (var suit in submarineParts)
+            {
+                foreach (var partName in suit)
+                {
+                    var partInfo = CrawlPage(partName);
+                    partInfo.Name = partName;
+                }
+            }
         }
 
         /// <summary>
@@ -240,5 +271,41 @@ namespace Kuranado.Moe.FFXIV
             var doc = new XmlDocument();
             doc.Load(dataPath);
         }
-    }
-}
+
+        /// <summary>
+        /// 爬取并解析部件信息
+        /// </summary>
+        /// <param name="link">要爬取的页面链接</param>
+        /// <returns></returns>
+        private static PartInfo CrawlPage(string link)
+        {
+            var result = new PartInfo();
+
+            var html = new HtmlWeb().Load(link);
+            var partInfoXPath = @"//div[@class=""infobox-item ff14-content-box""]";
+            var craftXPath = @"//div[@class=""item-craft-table-list filter-div""]//table/tr[4]/td/ul/li/ul";
+
+            //  part info
+            var partInfoHtml = html.DocumentNode.SelectSingleNode(partInfoXPath);
+            var regex = new Regex(partInfoPattern, RegexOptions.Compiled | RegexOptions.ExplicitCapture | RegexOptions.IgnorePatternWhitespace);
+            var match = regex.Match(partInfoHtml.OuterHtml);
+            result.UsingLv = byte.Parse(match.Groups["lv"].Value);
+            result.Weight = byte.Parse(match.Groups["weight"].Value);
+            result.ExplorePerf = short.Parse(match.Groups["explore"].Value);
+            result.CollectionPerf = short.Parse(match.Groups["collection"].Value);
+            result.CruisingSpeed = short.Parse(match.Groups["speed"].Value);
+            result.SailingDistance = short.Parse(match.Groups["distance"].Value);
+            result.Lucky = short.Parse(match.Groups["lucky"].Value);
+            result.RepairCost = byte.Parse(match.Groups["repair"].Value);
+
+            //  craft info
+            var craftInfoHtml = html.DocumentNode.SelectSingleNode(craftXPath);
+            foreach(var craftItem in craftInfoHtml.ChildNodes)
+            {
+                var innerText = craftItem.ChildNodes[0].InnerText;
+            }
+
+            return result;
+        }
+    }// end of class
+}// end of namespace
